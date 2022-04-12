55835d44fd8a5db98e302bbd52a554cd428ca160
Lucas Morón <lucasmoron0512@gmail.com>
2022-04-11 21:27:44 -0300

    Spring Initializr

---
+++ src/main/kotlin/ar/edu/unq/ttip/grupo1s12022/OdontoTTIPbackend/OdontoTtiPbackendApplication.kt
@@ -0,0 +1,11 @@
+package ar.edu.unq.ttip.grupo1s12022.OdontoTTIPbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OdontoTtiPbackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<OdontoTtiPbackendApplication>(*args)
+}
---
+++ src/test/kotlin/ar/edu/unq/ttip/grupo1s12022/OdontoTTIPbackend/OdontoTtiPbackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package ar.edu.unq.ttip.grupo1s12022.OdontoTTIPbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OdontoTtiPbackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
