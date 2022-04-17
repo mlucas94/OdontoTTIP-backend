2ad7544cb59a8bc774c81e18a404a7ae1647ffd1
Lucas Morón <lucasmoron0512@gmail.com>
2022-04-17 03:00:41 -0300

    Script de carga de datos iniciales modificado

--- src/main/resources/db/migration/V1_0_0__create_poc_data.sql
+++ src/main/resources/db/migration/V1_0_0__create_poc_data.sql
@@ -1,14 +1,11 @@
---DROP TABLE flyway_schema_history;
+--DROP TABLE flyway_schema_history
 
 CREATE TABLE turnos
 (
     id       INT NOT NULL KEY AUTO_INCREMENT,
-    paciente INT,
+    dni VARCHAR(10),
+    nombre VARCHAR(255),
+    email VARCHAR(255),
+    telefono VARCHAR(15),
     fecha    DATETIME
-)
-
-CREATE TABLE paciente
-(
-    id INT NOT NULL KEY AUTO_INCREMENT
-        nombre VARCHAR
 );
